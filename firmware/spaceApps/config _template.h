c60b68f26681ad8a6fef92eb6f81619e7d54b6bc
H-S-S-11 <harry@snell.org.uk>
2020-10-04 18:43:59 +0100

    template config file

---
+++ firmware/spaceApps/config _template.h
@@ -0,0 +1,7 @@
+//Rename the file to config.h, and fill in the required information below
+#define SSID_LOCAL "your_ssid"
+#define WLAN_KEY "your_wifi_password"
+
+//Address of the http server running the web app with /data on the end
+//IP should be shown when running http-server
+#define LOCAL_SERVER "http://192.168.1.33:8080/data"
--- firmware/spaceApps/spaceApps.ino
+++ firmware/spaceApps/spaceApps.ino
@@ -22,18 +22,9 @@ Servo servo;
 void setup() {
   Serial.begin(115200);
   Serial.println();
-
-  /* this part probably isn't necessary as the esp8266 will act as a client
-  IPAddress STATIC_IP;
-  IPAddress GATEWAY;
-  IPAddress SUBNET_MASK;
-  WiFi.config(ip, gateway, subnet_mask);
-  WiFi.mode(WIFI_STA);
-  */  
   
   WiFi.begin(SSID_LOCAL, WLAN_KEY);
   
-
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
   {
