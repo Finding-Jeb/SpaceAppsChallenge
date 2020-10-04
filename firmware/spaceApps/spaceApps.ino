ded12904a8d96fd0b8e501feb5b4a4bd62d52d8a
H-S-S-11 <harry@snell.org.uk>
2020-10-04 03:56:04 +0100

    add support for stepper motor

--- firmware/spaceApps/spaceApps.ino
+++ firmware/spaceApps/spaceApps.ino
@@ -1,19 +1,21 @@
+#include <CheapStepper.h>
+#include <Servo.h>
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include "config.h"
 
 HTTPClient data_client;
-int compass;
-int elevation;
+int compass = 0;
+int elevation = 0;
 
 //https://arduinojson.org/v6/assistant/
 const size_t capacity = JSON_OBJECT_SIZE(2) + 20;    
 DynamicJsonDocument doc(capacity);    
 
-
-
-char readBuffer[40];
+//Motors
+CheapStepper stepper(2, 4, 5, 16);
+bool moveClockwise = true;
 
 void setup() {
   Serial.begin(115200);
@@ -45,25 +47,57 @@ void setup() {
   data_client.begin(LOCAL_SERVER);
   Serial.println("Connected to local server");
   
+
+  /*Motor setup */
+  stepper.setRpm(12); //Fairly low for higher torque
   
+  Serial.println();
+  Serial.println("Stepper motor:");
+  Serial.print(stepper.getRpm()); // get the RPM of the stepper
+  Serial.print(" rpm = delay of ");
+  Serial.print(stepper.getDelay()); // get delay between steps for set RPM
+  Serial.println(" microseconds between steps");
+
+  //go the middle: South calibration
+  
+ 
 }
 
 void loop() {
 
-  char httpCode = data_client.GET();
-  if(httpCode){
-      
-    //https://arduinojson.org/v6/assistant/  
-    String json = data_client.getString(); 
-    deserializeJson(doc, json);    
-    compass = doc["compass"];
-    elevation = doc["elevation"]; 
+  stepper.run();
 
-    Serial.print("compass = ");
-    Serial.print(compass);
-    Serial.print(", elevation = ");
-    Serial.println(elevation);
-    
-  } 
+  int stepsLeft = stepper.getStepsLeft();
+
+  if(!stepsLeft){
+    //Get data from server
+    char httpCode = data_client.GET();
+    if(httpCode){
+        
+      //https://arduinojson.org/v6/assistant/  
+      String json = data_client.getString(); 
+      deserializeJson(doc, json);
+
+      if(doc["compass"] >= compass){
+        moveClockwise = true;
+      }
+      else{
+        moveClockwise = false;
+      }
+      compass = doc["compass"];
+      elevation = doc["elevation"]; 
+  
+      //Serial.print("compass = ");
+      //Serial.print(compass);
+      //Serial.print(", elevation = ");
+      //Serial.println(elevation);    
+    } 
+
+    //Use data to drive motors
+    stepper.newMoveToDegree(moveClockwise, compass);
+  }
+
+  delay(1);
+  
 
 }
